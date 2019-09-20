db3e76ebc031f62e4e60090d42615bc1c6621d52
jules viot <jules.viot2@gmail.com>
2019-09-20 19:05:06 +0200

    Initial Commit

---
+++ CultureG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CultureG
+//
+//  Created by Jules Viot on 20/09/2019.
+//  Copyright © 2019 Jules Viot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
